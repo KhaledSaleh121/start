88a621692aa44440bc684168e071bbf8a13e02a2
Khaled Saleh <71279851+KhaledSaleh122@users.noreply.github.com>
2022-11-15 22:38:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Puppeteer = require("puppeteer")
 const bot = async()=>{
     const browser = await Puppeteer.launch({
+        executablePath: __dirname+"/chrome/win64-1056772/chrome-win/chrome.exe",
         args:['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
